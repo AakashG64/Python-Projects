2677b6cf4c64e63d743754e21708b0c8d1440f52
Aakash Garg <aakashgarg640@gmail.com>
2022-05-17 17:14:03 +0530

    Initial Commit

---
+++ Python OpenCV/emotiondetection.py
@@ -0,0 +1,11 @@
+# Face Emotion Recognition App
+'''
+Deep Face is a pre-trained model.
+DeepFace is a deep learning facial recognition system created by a research group at Facebook.
+It identifies human faces in digital images.
+The program employs a nine-layer neural network with over 120 million connection weights and
+was trained on four million images uploaded by Facebook users.
+'''
+
+import cv2
+
